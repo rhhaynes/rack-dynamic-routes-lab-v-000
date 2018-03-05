72a7c38518a9036ecc54b13fb03e5eea00cb058c
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-05 21:08:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,8 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split(/items/).last
       
+      puts item_name
+      
       if item = Item.all.find{|item| item.name==item_name}
         resp.status = 200
         resp.write("#{item.price}\n")
