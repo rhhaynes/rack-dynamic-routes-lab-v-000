e6c58029be1a3b937540870488d76b86cc7e688c
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-05 20:44:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,14 +6,17 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split(/items/).last
+      
       if Items.all.include?(item_name)
+        resp.status = 200
         resp.write()
       else
         resp.status = 400
-        resp.write()
+        resp.write("Item not found")
       end
+      
     else
-      req.status = 404
+      resp.status = 404
     end
     
     resp.finish
