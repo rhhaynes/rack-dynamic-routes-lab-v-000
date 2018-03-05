7481923edc2513efdf462cadf39dbb3279289021
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-05 20:47:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,9 +7,10 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split(/items/).last
       
-      if Items.all.include?(item_name)
+      if Items.all.any{|item| item.name==item_name}
+        item = Items.all.find{|item| item.name==item_name}
         resp.status = 200
-        resp.write()
+        resp.write(item.price)
       else
         resp.status = 400
         resp.write("Item not found")
