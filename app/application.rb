92d5a42aaac9798d23ce55c0bfa641f73aafe339
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-05 20:42:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,12 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split(/items/).last
-      
+      if Items.all.include?(item_name)
+        resp.write()
+      else
+        resp.status = 400
+        resp.write()
+      end
     else
       req.status = 404
     end
